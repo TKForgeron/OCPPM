246a035b89f9fd534d1fb9c9ee3144b4eeb22803
TKForgeron <tkforgeron@gmail.com>
2023-06-22 12:00:21 +0200

    object feature graph encoding ready and first run in

--- experiment/feature_encodings/ofg/construct_bpi17_ofg.py
+++ experiment/feature_encodings/ofg/construct_bpi17_ofg.py
@@ -1,21 +1,22 @@
 # %%
 # import logging
 import pickle
-from typing import Any, Callable, Optional
+from typing import Any
 
+import hetero_graph_encoding_utils as utils
 import numpy as np
 import pandas as pd
 import pm4py
 import pm4py.ocel
 import pm4py.read
 import torch
-import utils
 from pm4py.algo.transformation.ocel.features.objects import (
     algorithm as object_feature_factory,
 )
+from sklearn.preprocessing import StandardScaler
 from torch_geometric.data import HeteroData
 
-ocel_file = "data/BPI17/BPI2017-Final.jsonocel"
+ocel_file = "data/BPI17/source/BPI2017-Final.jsonocel"
 
 
 # %%
@@ -73,6 +74,10 @@ data, feature_names = object_feature_factory.apply(
 # %%
 # make pd.DataFrame from feature matrix
 object_features = pd.DataFrame(data, columns=feature_names)
+# NORMALIZE "@@object_lifecycle_duration" (JUST FOR TESTING)
+object_features.iloc[:, 1:2] = StandardScaler().fit_transform(
+    object_features.iloc[:, 1:2]
+)
 # retrieve the mapper from ocel object id to the object index in the pd.DataFrame (e.g. 'Offer_148581083':1)
 oid_object_index_map = utils.get_index_map(ocel.objects, "ocel:oid", "object_index")
 
@@ -98,7 +103,7 @@ application_attribute_feature_idxs = flatten(
         np.where(application_features.columns.str.contains(attr_name))[0]
         for attr_name in application_attributes["str"]
         + application_attributes["num"]
-        + ["object_index"]
+        + ["object_index", "object_lifecycle_duration"]
     ]
 )
 offer_attribute_feature_idxs = flatten(
@@ -106,7 +111,7 @@ offer_attribute_feature_idxs = flatten(
         np.where(offer_features.columns.str.contains(attr_name))[0]
         for attr_name in offer_attributes["str"]
         + offer_attributes["num"]
-        + ["object_index"]
+        + ["object_index", "object_lifecycle_duration"]
     ]
 )
 # subset application features, with correct columns
@@ -124,6 +129,7 @@ offer_features = utils.add_object_type_index(offer_features, "offer")
 object_index_offer_index_map = utils.get_index_map(
     offer_features, "object_index", "offer_index"
 )
+
 
 # %%
 # calculate object graph (we select object_interaction here, but other graphs are possible)
@@ -150,6 +156,7 @@ offer_to_node_map = utils.object_map_to_node_map(
     oid_object_index_map, object_index_offer_index_map, "offer"
 )
 
+
 # %%
 # rename edges to have correct edge_index for HeteroData
 bpi17_edges_per_edge_type = utils.rename_edges_in_split_dict(
@@ -160,8 +167,15 @@ bpi17_edges_per_edge_type = utils.rename_edges_in_split_dict(
 )
 
 # %%
-# define heteregeneous graph
+# define heterogeneous graph
 hetero_data = HeteroData()
+# define target variable for both "application" type and "offer" type
+hetero_data["application"].y = torch.tensor(
+    application_features["@@object_lifecycle_duration"].values
+)
+hetero_data["offer"].y = torch.tensor(
+    offer_features["@@object_lifecycle_duration"].values
+)
 # attach node feature vectors for both "application" type and "offer" type
 hetero_data["application"].x = torch.tensor(
     application_features.drop(["application_index", "object_index"], axis=1).values
@@ -178,9 +192,33 @@ hetero_data["application", "interacts", "offer"].edge_index = utils.to_torch_coo
     bpi17_edges_per_edge_type[("application", "offer")]
 )
 hetero_data["offer", "interacts", "offer"].edge_index = utils.to_torch_coo_format(
-    utils.to_undirected(bpi17_edges_per_edge_type[("offer", "offer")])
+    bpi17_edges_per_edge_type[("offer", "offer")]
+    # utils.to_undirected(bpi17_edges_per_edge_type[("offer", "offer")])
 )
 
+
 # %%
-with open("data/BPI17/feature_encodings/BPI17_OFG.pkl", "wb") as binary_file:
+# save HeteroData object
+with open(
+    "data/BPI17/feature_encodings/OFG/ofg/raw/BPI17_OFG.pkl", "wb"
+) as binary_file:
     pickle.dump(hetero_data, binary_file)
+
+# save object interaction graph information (for HOEG encoding)
+with open(
+    "data/BPI17/feature_encodings/HOEG/hoeg/raw/bpi17_ofg+oi_graph+app_node_map+off_node_map.pkl",
+    "wb",
+) as binary_file:
+    objects_data = {
+        "ofg": hetero_data,
+        "objects_interaction_graph": graph,
+        "object_feature_vector_map": {
+            "application": application_to_node_map,
+            "offer": offer_to_node_map,
+        },
+        "object_feature_matrices": {
+            "application": application_features,
+            "offer": offer_features,
+        },
+    }
+    pickle.dump(objects_data, binary_file)
