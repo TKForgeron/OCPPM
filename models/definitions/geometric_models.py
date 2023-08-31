8582ca49c21c03ca50cba6d7b258b618bb26d460
TKForgeron <tkforgeron@gmail.com>
2023-08-31 11:00:33 +0200

    update GNN designs, use exact same model across experiments and encodings

--- models/definitions/geometric_models.py
+++ models/definitions/geometric_models.py
@@ -3,10 +3,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch_geometric.nn as pygnn
 from torch.nn import BatchNorm1d, ModuleList, MSELoss, Sequential
-from torch_geometric.nn import (BatchNorm, GATConv, GATv2Conv, GCNConv,
-                                GeneralConv, GINConv, Linear, SAGEConv,
-                                TopKPooling, TransformerConv, global_add_pool,
-                                global_max_pool, global_mean_pool, to_hetero)
+from torch_geometric.nn import (
+    BatchNorm,
+    GATConv,
+    GATv2Conv,
+    GCNConv,
+    GeneralConv,
+    GINConv,
+    Linear,
+    SAGEConv,
+    TopKPooling,
+    TransformerConv,
+    global_add_pool,
+    global_max_pool,
+    global_mean_pool,
+    to_hetero,
+)
 
 
 class GraphModel(torch.nn.Module):
@@ -18,50 +30,33 @@ class GraphModel(torch.nn.Module):
 class AdamsGCN(GraphModel):
     """Implementation of a Graph Convolutional Network as in Adams et al. (2022)"""
 
-    def __init__(self, num_node_features: int, hyperparams: dict):
+    def __init__(
+        self,
+        hidden_channels: int = 24,
+        out_channels: int = 1,
+        regression_target: bool = True,
+        graph_level_prediction: bool = True,
+        squeeze: bool = False,
+    ):
         super().__init__()
-        self.hyperparams = hyperparams
-        self.gconv1 = GCNConv(num_node_features, hyperparams["num_hidden_features"])
-        self.gconv2 = GCNConv(
-            hyperparams["num_hidden_features"], hyperparams["num_hidden_features"]
-        )
-        self.out = Linear(
-            hyperparams["num_hidden_features"] * hyperparams["size_subgraph_samples"], 1
-        )
+        self.squeeze = squeeze
+        self.conv1 = GCNConv(-1, hidden_channels)
+        self.conv2 = GCNConv(-1, hidden_channels)
+        self.pool1 = global_mean_pool
+        self.lin_out = Linear(-1, out_channels)
 
-    def forward(self, x, edge_index):
-        # x, edge_index = data.x, data.edge_index
-
-        # First Message Passing Layer (Transformation)
-        x = self.gconv1(x, edge_index)
+    def forward(self, x, edge_index, batch=None):
+        x = self.conv1(x, edge_index)
         x = x.relu()
-
-        # Second Message Passing Layer
-        x = self.gconv2(x, edge_index)
+        x = self.conv2(x, edge_index)
         x = x.relu()
-
-        # Reshape layer, to account for graph-level predictions,
-        # since we're given concatenated subgraph samples each mini batch
-        x = torch.reshape(
-            x,
-            (
-                int(x.shape[0] / self.hyperparams["size_subgraph_samples"]),
-                int(
-                    x.shape[0]
-                    * x.shape[1]
-                    / (x.shape[0] / self.hyperparams["size_subgraph_samples"])
-                ),
-            ),
-        )
-
-        # Output layer
-        out = self.out(x)
-        return out
+        x = self.pool1(x, batch)
+        x = self.lin_out(x)
+        x = torch.squeeze(x) if self.squeeze else x
+        return x
 
 
 class SimpleGNN_EFG(GraphModel):
-    """Implementation of a Graph Convolutional Network as in Adams et al. (2022)"""
-
     # SimpleGNN_EFG(64, 1): 0.4382 MAE (test), 6k params
     def __init__(
         self,
@@ -157,40 +152,7 @@ class SimpleHigherOrderGNN_EFG(GraphModel):
         return self.probs_out(x)
 
 
-class HigherOrderGNN_EFG(GraphModel):
-    """
-    Implementation of a Higher Order Graph Neural Network for EFG,
-    presented by Morris et al. (2019). This type of GNN operator is
-    especially suitable for graph-level prediction tasks.
-
-    The authors show that GNNs have the same expressiveness as the
-    1-WL in terms of distinguishing non-isomorphic (sub-)graphs,
-    and propose a generalization of GNNs, called k-dimensional GNNs
-    (k-GNNs), which can take higher-order graph structures at multiple
-    scales into account.
-
-    The "k" in k-dimensional refers to the number of scales or levels of
-    higher-order structures that the network can capture.
-        For example, a 2-dimensional GNN could capture both pairwise
-        connections between nodes (1st order) and communities of nodes
-        (2nd order), while a 3-dimensional GNN could capture even more
-        complex patterns that involve groups of communities (3rd order).
-
-    In this context, higher-order graph structures refer to patterns
-    or relationships that exist between groups of nodes in a graph,
-    beyond just the pairwise connections between individual nodes.
-        For example, in a social network, higher-order structures
-        could include communities of users who interact with each
-        other more frequently than with users outside of their community.
-        In a molecule graph, higher-order structures could include
-        functional groups that are composed of multiple atoms and have
-        specific chemical properties.
-    By taking these higher-order structures into account, k-dimensional
-    GNNs can capture more complex relationships between nodes in a graph
-    and potentially improve the accuracy of graph classification and
-    regression tasks.
-    """
-
+class HigherOrderGNN_EFG_SG(GraphModel):
     # HigherOrderGNN_EFG(48, 1): 0.4040 MAE (test)
 
     def __init__(
@@ -223,39 +185,84 @@ class HigherOrderGNN_EFG(GraphModel):
         return self.probs_out(x)
 
 
-class HeteroHigherOrderGNN(GraphModel):
-    """Implementation of a Attentional Graph Neural Network for EFG"""
+class HigherOrderGNN(GraphModel):
+    """
 
-    # For HOEG: HeteroHigherOrderGNN(32, 1) yields 0.1849 MAE (test)
-    # For OFG: HeteroHigherOrderGNN(64, 1) yields 0.7120 MAE (test)
+    Implementation of a Higher Order Graph Neural Network for node-level
+    prediction, presented by Morris et al. (2019). This type of GNN
+    operator is especially suitable for graph-level prediction tasks.
+
+    The authors show that GNNs have the same expressiveness as the
+    1-WL in terms of distinguishing non-isomorphic (sub-)graphs,
+    and propose a generalization of GNNs, called k-dimensional GNNs
+    (k-GNNs), which can take higher-order graph structures at multiple
+    scales into account.
+
+    The "k" in k-dimensional refers to the number of scales or levels of
+    higher-order structures that the network can capture.
+        For example, a 2-dimensional GNN could capture both pairwise
+        connections between nodes (1st order) and communities of nodes
+        (2nd order), while a 3-dimensional GNN could capture even more
+        complex patterns that involve groups of communities (3rd order).
+
+    In this context, higher-order graph structures refer to patterns
+    or relationships that exist between groups of nodes in a graph,
+    beyond just the pairwise connections between individual nodes.
+        For example, in a social network, higher-order structures
+        could include communities of users who interact with each
+        other more frequently than with users outside of their community.
+        In a molecule graph, higher-order structures could include
+        functional groups that are composed of multiple atoms and have
+        specific chemical properties.
+    By taking these higher-order structures into account, k-dimensional
+    GNNs can capture more complex relationships between nodes in a graph
+    and potentially improve the accuracy of graph classification and
+    regression tasks.
+
+    """
 
     def __init__(
         self,
         hidden_channels: int = 32,
         out_channels: int = 1,
+        no_preprocessing_layers: int = 0,
+        no_messagepassing_layers: int = 2,
+        no_postprocessing_layers: int = 1,
         squeeze: bool = False,
-        pre_forward_view:bool=False
+        graph_level_prediction: bool = False,
     ):
         super().__init__()
         self.squeeze = squeeze
-        self.pre_forward_view = pre_forward_view
-
-        self.conv1 = pygnn.GraphConv(-1, hidden_channels)
-        self.conv2 = pygnn.GraphConv(-1, hidden_channels)
-        self.act1 = nn.PReLU()
-        self.act2 = nn.PReLU()
-        self.lin_out = pygnn.Linear(-1, out_channels)
-
+        self.pre_layers = nn.ModuleList()
+        self.convs = nn.ModuleList()
+        self.acts = nn.ModuleList()
+        self.post_layers = nn.ModuleList()
+        for _ in range(no_preprocessing_layers):
+            self.pre_layers.append(pygnn.Linear(-1, hidden_channels))
+        for _ in range(no_messagepassing_layers):
+            self.convs.append(pygnn.GraphConv(-1, hidden_channels))
+            self.acts.append(nn.PReLU())
+        self.gpool = lambda x, batch: x
+        if graph_level_prediction:
+            self.gpool = pygnn.global_add_pool
+        for i in range(no_postprocessing_layers):
+            if i != (no_postprocessing_layers - 1):
+                self.post_layers.append(pygnn.Linear(-1, hidden_channels))
+            else:
+                self.post_layers.append(pygnn.Linear(-1, out_channels))
 
     def forward(self, x, edge_index, batch=None):
-        x = x.view(1,-1) if self.pre_forward_view else x
-        x = self.conv1(x, edge_index)
-        x = self.act1(x)
-        x = self.conv2(x, edge_index)
-        x = self.act2(x)
-        x = self.lin_out(x)
+        for pre_processing in self.pre_layers:
+            x = pre_processing(x)
+        for conv, act in zip(self.convs, self.acts):
+            x = conv(x, edge_index)
+            x = act(x)
+        x = self.gpool(x, batch)
+        for post_processing in self.post_layers:
+            x = post_processing(x)
         x = torch.squeeze(x) if self.squeeze else x
         return x
+
 
 """
 What has been tried:
